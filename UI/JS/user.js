22b21a78cd3e24b96dd4a7b244138b43f5b55369
Idowu Adeleke <Idowu.Adeleke@VentureGardenGroup.Local>
2019-04-03 16:40:00 +0100

    chore(make all pages responsive): responsive design
    i made the site responsive on mobile, tablet and laptop devices
    
    [Delivers #164985283]

--- UI/JS/user.js
+++ UI/JS/user.js
@@ -237,6 +237,29 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+
+
+
   window.onclick = () => {
     if (event.target == debitModal) {
         debitModal.style.display = "none";
