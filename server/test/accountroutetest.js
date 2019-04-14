1e511dfc351d499854975b1fa4f3f00fd2e8a2d3
Your Name <you@example.com>
2019-04-14 01:13:15 +0100

    feat(staff can view specific bank account endpoint ): view specific bank accounts
    this feature enables staffs to view specific bank account
    
    [Delivers #165332080]

--- server/test/accountroutetest.js
+++ server/test/accountroutetest.js
@@ -224,4 +224,95 @@ describe('Test account related endpoints - POST, GET, PATH, DELETE', () => {
     });
   });
 
+  /**
+     * Test the GET /accounts/:accountNumber route
+     */
+  describe('GET /accounts/:accountNumber', () => {
+    it('it should throw an error if a client wants to get other user\'s account', (done) => {
+      const accountNumber = 45677988;
+      chai
+        .request(app)
+        .get(`/api/v1/accounts/${accountNumber}`)
+        .set('token', UserToken)
+        .end((err, res) => {
+          const { body } = res;
+          expect(body.status).to.be.equals(403);
+          expect(body).to.be.an('object');
+          expect(body.error).to.be.equals('only a staff has the permission to get other user\'s account');
+          done();
+        });
+    });
+
+    it('it should GET a bank account details as a client if i own the account', (done) => {
+      const accountNumber =  45678088;
+      chai
+        .request(app)
+        .get(`/api/v1/accounts/${accountNumber}`)
+        .set('token', UserToken)
+        .end((err, res) => {
+          const { body } = res;
+          expect(body.status).to.be.equals(200);
+          expect(body).to.be.an('object');
+          expect(body.data).to.haveOwnProperty('accountNumber');
+          expect(body.data).to.haveOwnProperty('firstName');
+          expect(body.data).to.haveOwnProperty('lastName');
+          expect(body.data).to.haveOwnProperty('email');
+          expect(body.data).to.haveOwnProperty('type');
+          expect(body.data).to.haveOwnProperty('balance');
+          done();
+        });
+    });
+
+    it('it should GET a bank account details as a staff', (done) => {
+      const accountNumber = 45678088;
+      chai
+        .request(app)
+        .get(`/api/v1/accounts/${accountNumber}`)
+        .set('token', adminToken)
+        .end((err, res) => {
+          const { body } = res;
+          expect(body.status).to.be.equals(200);
+          expect(body).to.be.an('object');
+          expect(body.data).to.haveOwnProperty('accountNumber');
+          expect(body.data).to.haveOwnProperty('firstName');
+          expect(body.data).to.haveOwnProperty('lastName');
+          expect(body.data).to.haveOwnProperty('email');
+          expect(body.data).to.haveOwnProperty('type');
+          expect(body.data).to.haveOwnProperty('balance');
+          done();
+        });
+    });
+
+    it('it should throw an error if a client account that doesnt exist',(done) => {
+      const accountNumber = 222567722988;
+      chai
+        .request(app)
+        .get(`/api/v1/accounts/${accountNumber}`)
+        .set('token', UserToken)
+        .end((err, res) => {
+          const { body } = res;
+          expect(body.status).to.be.equals(404);
+          expect(body).to.be.an('object');
+          expect(body.error).to.be.equals('account number doesn\'t exist');
+          done();
+        });
+    });
+    
+
+    it('it should throw an error when account number is not found', (done) => {
+      const accountNumber = 2220107724455; 
+      chai
+        .request(app)
+        .get(`/api/v1/accounts/${accountNumber}`)
+        .set('token', adminToken)
+        .end((err, res) => {
+          const { body } = res;
+          expect(body.status).to.be.equals(404);
+          expect(body).to.be.an('object');
+          expect(body.error).to.be.equals('account number doesn\'t exist');
+          done();
+        });
+    });
+  });
+
 });
