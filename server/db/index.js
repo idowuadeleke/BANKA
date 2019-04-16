0ff7dda748c0c485ad67ffca86b69733584378c5
Your Name <you@example.com>
2019-04-16 23:26:07 +0100

    chore(Database):set up database
    set up database connection so as to replace data structures previously used to store data
    
    [Finishes #165403183]

---
+++ server/db/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+let connectionString = process.env.DATABASE_URL;
+
+// Instantiate pool
+const pool = new Pool({
+  connectionString,
+});
+
+class Db {
+  /**
+   * query() queries database
+   *
+   * @param {string} queryString
+   * @param {*} params
+   */
+  static query(queryString, params) {
+    return new Promise((resolve, reject) => {
+      pool
+        .query(queryString, params)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+}
+
+export default Db;
