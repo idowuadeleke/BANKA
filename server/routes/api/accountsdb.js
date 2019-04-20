5fbbb4b4f50180b8add147c7b5d1b2964db584a5
Your Name <you@example.com>
2019-04-20 13:27:53 +0100

    chore(Database):migrate json to database
    migrate get all accounts and get specific account route to database
    
    [Finishes #165468703]

--- server/routes/api/accountsdb.js
+++ server/routes/api/accountsdb.js
@@ -5,13 +5,15 @@ import checkPermission from '../../middleswares/checkPermission';
 
 const { verifyTokendb } = isAuth;
 
-const { permissionMiddleWare } = checkPermission;
+const { permissionMiddleWareDb } = checkPermission;
 
 const router = express.Router();
 
-const { createBankAccountdb } = accountController;
+const { createBankAccountdb,fetchAllAccountsDb, getAccountDb } = accountController;
 // user signup route
 router.post('/accounts', verifyTokendb, createBankAccountdb);
+router.get('/accounts', verifyTokendb, permissionMiddleWareDb, fetchAllAccountsDb);
+router.get('/accounts/:accountNumber', verifyTokendb, permissionMiddleWareDb, getAccountDb);
 
 
 // expose router
